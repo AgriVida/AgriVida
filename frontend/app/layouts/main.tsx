6cdaa450b4cb1392ded3938fae785ce0b3074775
Martin Poboril <mpoboril@cnm.edu>
2026-03-02 16:53:23 -0700

    navigation bar

--- frontend/app/layouts/main.tsx
+++ frontend/app/layouts/main.tsx
@@ -1,9 +1,11 @@
 import {Outlet} from "react-router"
 import Footer from "~/components/Footer";
+import {Navigation} from "~/components/Navigation";
+
 export default function MainLayout() {
     return (
         <>
-
+        <Navigation/>
         <Outlet/>
             <Footer />
         </>
